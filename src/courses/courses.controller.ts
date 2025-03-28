c4a11530f191f848bdb039e54a381cf0064903ea
eddwardmessias <eddward.messias@hotmail.com>
2025-03-27 21:01:46 -0300

    dtos and data validation

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -10,6 +10,8 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { CreateCourseDTO } from './dto/create-course.dto';
+import { UpdateCourseDTO } from './dto/update-course.dto';
 
 @Controller('courses')
 export class CoursesController {
@@ -24,7 +26,7 @@ export class CoursesController {
   @Get(':id')
   findOne(@Param('id') id: number) {
     console.log(id);
-    const teste = this.courseService.findOne(+id);
+    const teste = this.courseService.findOne(id);
     console.log(teste);
 
     return teste;
@@ -37,18 +39,18 @@ export class CoursesController {
   //   }
 
   @Post()
-  create(@Body() body) {
-    return this.courseService.create(body);
+  create(@Body() createCourseDTO: CreateCourseDTO) {
+    return this.courseService.create(createCourseDTO);
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() body) {
-    return this.courseService.update(+id, body);
+  update(@Param('id') id: number, @Body() updateCourseDTO: UpdateCourseDTO) {
+    return this.courseService.update(id, updateCourseDTO);
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
   delete(@Param('id') id: number) {
-    return this.courseService.remove(+id);
+    return this.courseService.remove(id);
   }
 }
