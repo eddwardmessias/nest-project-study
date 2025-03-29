0773b13696642ffdbbe6658f5fc54bdbf478cb62
eddwardmessias <eddward.messias@hotmail.com>
2025-03-29 11:10:00 -0300

    create migration tags

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1743255967692 } from 'src/migrations/1743255967692-CreateCoursesTable';
+import { CreateTagsTable1743256934914 } from 'src/migrations/1743256934914-CreateTagsTable';
 
 export const dataSorce = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1743255967692],
+  migrations: [CreateCoursesTable1743255967692, CreateTagsTable1743256934914],
 });
--- src/migrations/1743256934914-CreateTagsTable.ts
+++ src/migrations/1743256934914-CreateTagsTable.ts
@@ -1,7 +1,35 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateTagsTable1743256934914 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tags',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tags');
+  }
 }
